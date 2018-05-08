f1f1b36dd1bc9dde5f121889d9e58cdd39094942
Clement Trosa <me@trosa.io>
2018-05-08 13:37:28 +0200

    Updated dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject beepbop "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :description "42og group manager bot for newcomer. "
+  :url "https://github.com/42og/beepbop"
+  :license {:name "MIT License"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [tentacles-fork "0.6.0-SNAPSHOT"]]
   :main ^:skip-aot beepbop.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
